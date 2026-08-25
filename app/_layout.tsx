ca126d23344cf8de104037adafb76904e15d0f81
Elm-as <elmas@daloamarket.com>
2026-08-25 14:40:42 +0000

    chore: configure dedicated port 8082

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -45,6 +45,6 @@ export default function DeliveryRootLayout() {
           </Stack>
         </DriverAuthProvider>
       </QueryClientProvider>
-    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
